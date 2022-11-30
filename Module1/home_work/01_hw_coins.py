c82dd690ab11acf8dbed3008d66f3b862f861bc4
AndreyGigler <110337534+AndreyGigler@users.noreply.github.com>
2022-11-30 16:15:12 +0300

    Update 01_hw_coins.py

--- Module1/home_work/01_hw_coins.py
+++ Module1/home_work/01_hw_coins.py
@@ -1,15 +1,29 @@
+
+# Не понял пояснения к этой задаче. Результат получается верный, но не уверен, что правильно написал конструктор и метод
+
 import random
-
-
+n = int(input())
+coins_list = []
+head_list = []
+i = n
 class Coin:
     def __init__(self):
         self.side = None
 
     def flip(self) -> None:
-        """
-        Подбрасывание монетки. # heads-орел/tails-решка
-        """
-        self.side = ...  # random: heads/tails
+        random_list = ['head', 'tail']
+        random_choice = random.choice(random_list)
+        return random_choice
+while n <= 0:
+    print('Количество монет не может быть отрицательным или равняться нулю!')
+    n = int(input())
+while i > 0:
+    coins_list.append(Coin.flip(0))
+    i -= 1
+for coin in coins_list:
+    if coin == 'head':
+        head_list.append(coin)
+print(f"Аверс: ", ((len(head_list)*100)//n), f"%", f"Реверс: ", 100-(len(head_list)*100)//n, f"%")
 
 # Задание:
 # 1. Создайте список из n-монеток, n - вводится с клавиатуры
